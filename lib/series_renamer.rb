82e70d0a0ee6347516f9fbe3c0880c1952846b7b
Romain Fricon <romain.fricon@gmail.com>
2012-08-27 18:04:20 +0200

    add testing + make code testable

--- lib/series_renamer.rb
+++ lib/series_renamer.rb
@@ -7,17 +7,27 @@ class SerieRenamer
   # @param [String] file_to_rename The absolute path to the file
   # @author romain
   def self.rename_with_convention(file_to_rename)  
-    episode = File.basename(file_to_rename).to_s[/(?:E|eP?|x|-)(\d+)/i, 1] 
+    newname = filename_with_convention file_to_rename
+    File.rename(file_to_rename, newname) unless newname == file_to_rename
+  end
+  private
+  ##
+  # Generate the new filename with the convention: Title - S[season]E[episode].[extention]
+  # @param [String] file_to_rename The absolute path to the file
+  # @returns [String] the new filename who respects the convention
+  # @author romain
+  def filename_with_convention(old_filename)
+    episode = File.basename(old_filename).to_s[/(?:E|eP?|x|-)(\d+)/i, 1] 
     episode = "0#{episode}" if episode.length == 1
-    file_tab = file_to_rename.split "\/"
+    file_tab = old_filename.split "\/"
     season = file_tab[file_tab.length-2][/(\d+)/, 1] 
     season = "0#{season}" if season.length == 1
     title = file_tab[file_tab.length-3]
-    newname = "#{File.dirname(file_to_rename)}/#{title} - S#{season}E#{episode}#{File.extname(file_to_rename)}" 
+    newname = "#{File.dirname(old_filename)}/#{title} - S#{season}E#{episode}#{File.extname(old_filename)}" 
     if DEBUG
-      print "Old name: #{file_to_rename}\n New name: #{newname}\n"
-      puts "respecte la convention \n -------------------" if newname == file_to_rename 
+      print "Old name: #{old_filename}\n New name: #{newname}\n"
+      puts "respecte la convention \n -------------------" if newname == old_filename 
     end
-    File.rename(file_to_rename, newname) unless newname == file_to_rename
+    newname
   end
 end
--- test/test_series_renamer.rb
+++ test/test_series_renamer.rb
@@ -1,2 +1,21 @@
 require 'test/unit'
 require 'series_renamer'
+
+class Class
+  def publicize_methods
+    saved_private_instance_methods = self.private_instance_methods
+    self.class_eval { public *saved_private_instance_methods }
+    yield
+    self.class_eval { private *saved_private_instance_methods }
+  end
+end
+
+class SeriesRenamerTest < Test::Unit::TestCase
+	def test_filename_with_convention
+		file_path = "/test_series/Serie\ 1/Season\ 1/ep1.avi"
+		attended_file = "/test_series/Serie\ 1/Season\ 1/Serie\ 1\ -\ S01E01.avi"
+		SerieRenamer.publicize_methods do
+			assert_equal(attended_file, SerieRenamer.new.filename_with_convention(file_path))
+		end
+	end
+end
