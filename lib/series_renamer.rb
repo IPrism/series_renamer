4c02012723403724ba644212b08461137732d543
Romain Fricon <romain.fricon@gmail.com>
2013-09-07 14:44:29 +0200

    the method filename_with_convention returns now nil if the regex not match the episode

--- lib/series_renamer.rb
+++ lib/series_renamer.rb
@@ -8,7 +8,9 @@ class SeriesRenamer
   # @author romain
   def self.rename_with_convention(file_to_rename)  
     newname = self.filename_with_convention file_to_rename
-    File.rename(file_to_rename, newname) unless newname == file_to_rename
+    unless newname.nil?
+      File.rename(file_to_rename, newname) unless newname == file_to_rename
+    end
   end
   private
   ##
@@ -17,17 +19,20 @@ class SeriesRenamer
   # @returns [String] the new filename who respects the convention
   # @author romain
   def self.filename_with_convention(old_filename)
-    episode = File.basename(old_filename).to_s[/(?:E|eP?|x|-)(\d+)/i, 1] 
-    episode = "0#{episode}" if episode.length == 1
-    file_tab = old_filename.split "\/"
-    season = file_tab[file_tab.length-2][/(\d+)/, 1] 
-    season = "0#{season}" if season.length == 1
-    title = file_tab[file_tab.length-3]
-    newname = "#{File.dirname(old_filename)}/#{title} - S#{season}E#{episode}#{File.extname(old_filename)}" 
-    if DEBUG
-      print "Old name: #{old_filename}\n New name: #{newname}\n"
-      puts "respecte la convention \n -------------------" if newname == old_filename 
+    episode = File.basename(old_filename).to_s[/(?:E|eP?|x|-)(\d+)/i, 1]
+    unless episode.nil?
+      episode = "0#{episode}" if episode.length == 1
+      file_tab = old_filename.split "\/"
+      season = file_tab[file_tab.length-2][/(\d+)/, 1] 
+      season = "0#{season}" if season.length == 1
+      title = file_tab[file_tab.length-3]
+      newname = "#{File.dirname(old_filename)}/#{title} - S#{season}E#{episode}#{File.extname(old_filename)}" 
+      if DEBUG
+        print "Old name: #{old_filename}\n New name: #{newname}\n"
+        puts "respecte la convention \n -------------------" if newname == old_filename 
+      end
+      return newname
     end
-    newname
+    return nil 
   end
 end
